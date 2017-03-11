176740695b094e731fcf4a6d0348bbc218bd0f95
KKS161994 <kartikeyshrivastava@gmail.com>
2017-03-11 15:19:03 +0530

    Commiting toggle class for handling event with bind and () for function

--- src/Clock.js
+++ src/Clock.js
@@ -4,27 +4,49 @@ class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
+    this.handleClick = this.handleClick.bind(this);
   }
+  //Works once when the DOM is loaded first time
   componentDidMount() {
     this.timerID = setInterval(
         () => this.tick(),
         1000
       );
   }
+  
+
+
+
   componentDidUnmount() {
     clearInterval(this.timerID);
   }
+
+
   tick() {
     this.setState({
       date: new Date()
     });
   }
+  
+  //Handling events but it needs binding in the constructor
+  handleClick() {
+    console.log('This link was clicked');
+     clearInterval(this.timerID);
+  }
+
+  //Without adding binding to a function in constructo
+  clicked = () =>{
+  console.log('This link was clicked');
+     clearInterval(this.timerID);
+
+  }
   render() {
     return (
    <div className="App">
-        <div className="App-header">
-          </div>       <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            <button onClick={this.clicked}>
+            Fuck u
+            </button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Clock from './Clock';
+import Toggle from './Toggle';
+const element = (
+<div>
+    <div className="App-header">
+          </div>   
+<Clock />
+<Toggle />
+</div>	
+)
+;
 ReactDOM.render(
-  <Clock />,
+  element,
   document.getElementById('root')
 );
