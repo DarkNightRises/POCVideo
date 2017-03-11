935e255ce7b2e3bebef7b3caee502b4c027b5884
KKS161994 <kartikeyshrivastava@gmail.com>
2017-03-11 17:17:26 +0530

    Added conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,16 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Clock from './Clock';
 import Toggle from './Toggle';
+import LoginControl from './LoginControl';
 const element = (
 <div>
     <div className="App-header">
           </div>   
+          <h1>Working</h1>
+          
+
 <Clock />
 <Toggle />
+<LoginControl />
 </div>	
 )
 ;
 ReactDOM.render(
   element,
   document.getElementById('root')
+);
+//Conditional Rendering
+function UserGreeting(props) {
+	return <h1>Welcome back</h1>
+}
+function GuestGreeting(props) {
+	return <h1>Please SIgn Up</h1>
+}
+
+function Greeting(props) {
+	const isLoggedIn = props.isLoggedIn;
+	if (isLoggedIn){
+		return <UserGreeting />;
+
+	}
+	else {
+		return <GuestGreeting />;
+	}
+}
+const elem = (
+	<div>
+    <div className="App-header">
+          </div>   
+<Greeting isLoggedIn={true}/>
+</div>	);
+ReactDOM.render(
+elem,
+document.getElementById('greeting')
 );
