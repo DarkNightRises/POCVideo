724270eed3a5ff199d66f7c8c9bded2f541c3843
KKS161994 <kartikeyshrivastava@gmail.com>
2017-03-12 11:59:50 +0530

    Added key to list items map

--- src/index.js
+++ src/index.js
@@ -6,44 +6,50 @@ import Toggle from './Toggle';
 import LoginControl from './LoginControl';
 
 const element = (
-<div>
-    <div className="App-header">
-          </div>   
-          <h1>Working</h1>
-          
+		<div>
+		    <div className="App-header">
+		          </div>   
+		          <h1>Working</h1>
+		          
 
-<Clock />
-<Toggle />
-<LoginControl />
-</div>	
+		<Clock />
+		<Toggle />
+		<LoginControl />
+		</div>	
 );
-const numbers = [1,2,3,4,5];
-const list_items = numbers.map((number)=>
-<li>{number}</li>
-	);
-const new_element = (
-<div>
-    <div className="App-header">
-          </div>   
-<ul>{list_items}</ul>
-</div>
-	);
+		const numbers = [1,2,3,4,5];
+
+		const list_items = numbers.map((number)=>
+		<li>{number}</li>
+			);
+
+		const new_element = (
+					<div>
+					    <div className="App-header">
+					          </div>   
+					<ul>{list_items}</ul>
+					</div>
+			);
 ReactDOM.render(
-  new_element,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
 
-
-
-
-
+//Component for list 
+function NumberList(props) {
+	const numbers = props.numbers;
+	const listItems = numbers.map((number)=>
+		<li key={number.toString()}>{number}</li>
+		);
+	return (<ul>{listItems}</ul>);
+}
 
 //Conditional Rendering
 function UserGreeting(props) {
-	return <h1>Welcome back</h1>
+	return <h1>Welcome back</h1>;
 }
 function GuestGreeting(props) {
-	return <h1>Please SIgn Up</h1>
+	return <h1>Please SIgn Up</h1>;
 }
 
 function Greeting(props) {
